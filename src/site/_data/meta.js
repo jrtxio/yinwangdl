2d46c1316ce9fc3a55c6b7d044ba250fc6dd794c
jrtxio <jirentianxiang1024@gmail.com>
2026-04-29 21:48:21 +0800

    fix: hardcode AdSense IDs to fix ads not showing
    Ads were gated behind environment variables that weren't set on Vercel.
    Hardcode the IDs like the other blog (jrtxio-obsidian-blogs) does.

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -4,7 +4,5 @@ module.exports = () => ({
   siteName: process.env.SITE_NAME || "王垠博客备份",
   siteBaseUrl: process.env.SITE_BASE_URL || "",
   mainLanguage: process.env.SITE_MAIN_LANGUAGE || "zh",
-  adsenseClient: process.env.GOOGLE_ADSENSE_CLIENT || "",
-  adsenseSlot: process.env.GOOGLE_ADSENSE_SLOT || "",
   buildDate: new Date(),
 });
